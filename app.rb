57cfdb7255485833003ec57a62fa3037bea2f478
jioluvalis <juansivas@outlook.com>
2021-08-13 11:44:07 -0500

    view ready for testing

--- app.rb
+++ app.rb
@@ -13,10 +13,9 @@ get '/' do
   logger = Logger.new(STDOUT)
   logger.info(request)
   @name = "PORTWORX"
-  respuesta_1 = []
-  respuesta_2 = []
+  respuesta = []
   response.set_cookie("llave", value: "valor")
-  erb :portworx , :locals => {:respuesta_1 => respuesta_1,:respuesta_2 => respuesta_2}
+  erb :portworx , :locals => {:respuesta => respuesta}
 end
 
 get '/portworx-precio' do
@@ -25,58 +24,43 @@ get '/portworx-precio' do
   logger.info("Parámetros para dimensionamiento portworx:\n")
 
   #CLUSTER IKS/OCP
-  cluster_type_1="#{params['cluster_type_1']}"
-  wn_1="#{params['wn_1']}"
-  infra_type_1="#{params['infra_type_1']}"
-  flavor_1="#{params['flavor_1']}"
-  region_cluster_1="#{params['region_cluster_1']}"
+  cluster_type_prod="#{params['cluster_type_prod']}"
+  wn_prod="#{params['wn_prod']}"
+  infra_type_prod="#{params['infra_type_prod']}"
+  flavor_prod="#{params['flavor_prod']}"
+  region_cluster_prod="#{params['region_cluster_prod']}"
 
-  cluster_type_2="#{params['cluster_type_2']}"
-  wn_2="#{params['wn_2']}"
-  infra_type_2="#{params['infra_type_2']}"
-  flavor_2="#{params['flavor_2']}"
-  region_cluster_2="#{params['region_cluster_2']}"
+  cluster_type_dr="#{params['cluster_type_dr']}"
+  wn_dr="#{params['wn_dr']}"
+  infra_type_dr="#{params['infra_type_dr']}"
+  flavor_dr="#{params['flavor_dr']}"
+  region_cluster_dr="#{params['region_cluster_dr']}"
 
-  logger.info("Clúster 1: tipo de cluster: #{cluster_type_1}, wn: #{wn_1}, tipo de infra: #{infra_type_1}, flavor: #{flavor_1}, región: #{region_cluster_1}")
-  logger.info("Clúster 2: tipo de cluster: #{cluster_type_2}, wn: #{wn_2}, tipo de infra: #{infra_type_2}, flavor: #{flavor_2}, región: #{region_cluster_2}")
+  logger.info("Clúster prod: tipo de cluster: #{cluster_type_prod}, wn: #{wn_prod}, tipo de infra: #{infra_type_prod}, flavor: #{flavor_prod}, región: #{region_cluster_prod}")
+  logger.info("Clúster dr: tipo de cluster: #{cluster_type_dr}, wn: #{wn_dr}, tipo de infra: #{infra_type_dr}, flavor: #{flavor_dr}, región: #{region_cluster_dr}")
   #BLOCK STORAGE
-  storage_block="#{params['storage_block_1']}"
-  iops="#{params['iops_1']}"
+  storage_block="#{params['storage_block']}"
+  iops="#{params['iops']}"
 
-  logger.info("Storage 1: storage(GB): #{storage_block}, iops: #{iops}, region: #{region_cluster_1}")
-  logger.info("Storage 2: storage(GB): #{storage_block}, iops: #{iops}, region: #{region_cluster_2}")
+  logger.info("Storage: storage(GB): #{storage_block}, iops: #{iops}, region: #{region_cluster_prod}")
   #DB FOR ETCD
-  ram_etcd="#{params['ram_etcd_1']}"
-  storage_etcd="#{params['storage_etcd_1']}"
-  cores="#{params['cores_1']}"
+  ram_etcd="#{params['ram_etcd']}"
+  storage_etcd="#{params['storage_etcd']}"
+  cores="#{params['cores']}"
+  region_etcd="#{params['region_etcd']}"
 
-  logger.info("DB ETCD 1: ram(GB): #{ram_etcd}, storage: #{storage_etcd}, cores: #{cores} region: #{region_cluster_1}")
-  logger.info("DB ETCD 2: ram(GB): #{ram_etcd}, storage: #{storage_etcd}, cores: #{cores} region: #{region_cluster_2}")
-
-  
-  respuestasizing=[]
-  respuestasizingalt=[]
-  respuestaprecio=[]
-
+  logger.info("DB ETCD: ram(GB): #{ram_etcd}, storage: #{storage_etcd}, cores: #{cores} region: #{region_etcd}")
 
   urlapi="https://apis-portworx.9sxuen7c9q9.us-south.codeengine.appdomain.cloud"
 
-  request = "#{urlapi}/api/lvl2/portworxsol?tipo_cluster=#{cluster_type_1}&wn=#{wn_1}&region_cluster=#{region_cluster_1}&infra_type=#{infra_type_1}&flavor=#{flavor_1}&iops=#{iops}&region_storage=#{region_cluster_1}&storage=#{storage_block}&region_etcd=#{region_cluster_1}&ram_etcd=#{ram_etcd}&storage_etcd=#{storage_etcd}&cores_etcd=#{cores}&region_portworx=dallas"
+  request = "#{urlapi}/api/lvl2/portworxsol?tipo_cluster_prod=#{cluster_type_prod}&wn_prod=#{wn_prod}&region_cluster_prod=#{region_cluster_prod}&infra_type_prod=#{infra_type_prod}&flavor_prod=#{flavor_prod}&tipo_cluster_dr=#{cluster_type_dr}&wn_dr=#{wn_dr}&region_cluster_dr=#{region_cluster_dr}&infra_type_dr=#{infra_type_dr}&flavor_dr=#{flavor_dr}&iops=#{iops}&region_storage=#{region_cluster_prod}&storage=#{storage_block}&region_etcd=#{region_etcd}&ram_etcd=#{ram_etcd}&storage_etcd=#{storage_etcd}&cores_etcd=#{cores}"
   logger.info(request)
-  respuesta_1 = RestClient.get "#{request}", {:params => {}}
-  respuesta_1 = JSON.parse(respuesta_1.to_s)
-  logger.info(respuesta_1)
-  jsonobj = respuesta_1.to_json
-  logger.info("Valor en JSON")
-  logger.info(jsonobj)
+  respuesta = RestClient.get "#{request}", {:params => {}}
+  respuesta = JSON.parse(respuesta.to_s)
+  logger.info(respuesta)
+  
 
-  request = "#{urlapi}/api/lvl2/portworxsol?tipo_cluster=#{cluster_type_2}&wn=#{wn_2}&region_cluster=#{region_cluster_2}&infra_type=#{infra_type_2}&flavor=#{flavor_2}&iops=#{iops}&region_storage=#{region_cluster_2}&storage=#{storage_block}&region_etcd=#{region_cluster_2}&ram_etcd=#{ram_etcd}&storage_etcd=#{storage_etcd}&cores_etcd=#{cores}&region_portworx=dallas"
-  logger.info(request)
-  respuesta_2 = RestClient.get "#{request}", {:params => {}}
-  respuesta_2 = JSON.parse(respuesta_2.to_s)
-  logger.info(respuesta_2)
-
-  erb :portworx , :locals => {:respuesta_1 => respuesta_1, :respuesta_2 => respuesta_2}
+  erb :portworx , :locals => {:respuesta => respuesta}
 end
 
 
